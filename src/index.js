69f4d979d8c93b6e8a07d15f931454facda2a47a
Mariusz Kaczkowski <mariusz.kaczkowski@dcx.bz>
2018-08-31 16:14:59 +0200

    service-worker updated

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// eslint-disable-next-line func-names,wrap-iife,space-before-function-paren
-(function() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js');
-  }
-})();
+registerServiceWorker();
