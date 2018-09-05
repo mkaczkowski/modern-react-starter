1b80bfaf9847c1f35c2e23de85a301871a174f51
Mariusz Kaczkowski <mariusz.kaczkowski@dcx.bz>
2018-09-05 17:20:19 +0200

    merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
-import Skills from './view/skills';
 import Hero from './view/hero/Hero';
-import Introduction from './view/introduction';
-import Contact from './view/contact';
 
 import './styles/index.css';
 
@@ -11,9 +8,6 @@ function App() {
   return (
     <main>
       <Hero />
-      <Introduction />
-      <Skills />
-      <Contact />
     </main>
   );
 }
