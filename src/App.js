a955ab05d649db5d5fda2d68f5856203e38fe4f6
Mariusz Kaczkowski <mariusz.kaczkowski@dcx.bz>
2018-09-06 13:11:27 +0200

    flow-types added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
+import Header from './components/header';
 import Skills from './view/skills';
 import Hero from './view/hero/Hero';
 import Introduction from './view/introduction';
 import Contact from './view/contact';
-import Header from './components/Header';
 
 import './styles/index.css';
 
